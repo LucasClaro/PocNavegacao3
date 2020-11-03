74f5625fe9b718d571fdbe96930478e06ecb73be
Lucas Claro <lucasbrclaro@gmail.com>
2020-11-03 17:24:38 -0300

    Initial Commit

---
+++ PocNavegacao3/PocNavegacao3App.swift
@@ -0,0 +1,17 @@
+//
+//  PocNavegacao3App.swift
+//  PocNavegacao3
+//
+//  Created by Lucas Claro on 03/11/20.
+//
+
+import SwiftUI
+
+@main
+struct PocNavegacao3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
